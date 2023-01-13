454da385991aab3b85c6c095ac95c1c74365aedd
Jeronimo Grandi <JGGRANDI@uncg.edu>
2023-01-13 18:53:05 -0500

    Added tracker and testing new ways to do the rotation matching

--- Assets/RotationMatching/Scripts/MatchingAngles.cs
+++ Assets/RotationMatching/Scripts/MatchingAngles.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 public class MatchingAngles : MonoBehaviour
 {
 
-    [SerializeField] private GameObject rhand, lhand;
+    [SerializeField] private GameObject object1, object2;
 
     [SerializeField] private float currentValue = 0f;
     [SerializeField] private float referenceValue = 0f;
 
     [Range(.001f, 1f)]
     [SerializeField] private float threashold = .1f;
+
+    private int sample = 0;
+    private int maxSample = 500;
+    private float prevValue = 0f;
 
 
     void Start()
@@ -19,24 +23,37 @@ public class MatchingAngles : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.V))
+        //if (Input.GetKeyDown(KeyCode.V))
+        //    referenceValue = currentValue;
+
+        var obj1matrix = Matrix4x4.TRS(new Vector3(0, 0, 0), object1.transform.rotation, new Vector3(1.0f, 1.0f, 1.0f));
+        var obj2matrix = Matrix4x4.TRS(new Vector3(0, 0, 0), object2.transform.rotation, new Vector3(1.0f, 1.0f, 1.0f));
+
+        currentValue = Utils.DistMatrices(obj1matrix, obj2matrix);
+        
+        var absdiff = Mathf.Abs(referenceValue - currentValue);
+
+        prevValue = currentValue;
+        var absdiffCurrPrev = Mathf.Abs(prevValue - currentValue);
+
+
+        if (absdiffCurrPrev < threashold)
+            sample++;
+        else
+            sample = 0;
+
+        Debug.Log(sample);
+
+        if (sample > maxSample)
             referenceValue = currentValue;
 
-        var rhandmatrix = Matrix4x4.TRS(new Vector3(0, 0, 0), rhand.transform.rotation, new Vector3(1.0f, 1.0f, 1.0f));
-        var lhandmatrix = Matrix4x4.TRS(new Vector3(0, 0, 0), lhand.transform.rotation, new Vector3(1.0f, 1.0f, 1.0f));
-
-        currentValue = Utils.DistMatrices(rhandmatrix, lhandmatrix);
-        
-        var diff = referenceValue - currentValue;
-
-        if (Mathf.Abs(diff) > threashold)
+        if (absdiff > threashold)
         {
-            Debug.Log("NOT MATCHING --- Diff: " + diff);
+
+            Debug.Log("NOT MATCHING --- Diff: " + absdiff);
             return;
         }
 
-
-
-        Debug.Log("    MATCHING --- Diff: " + diff);
+        Debug.Log("    MATCHING --- Diff: " + absdiff);
     }
 }
