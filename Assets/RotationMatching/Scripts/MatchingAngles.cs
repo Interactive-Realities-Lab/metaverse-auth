2e5b2d2d9344d5a08ea958d2dcb0b5eaf86faf47
jggrandi <jggrandi@gmail.com>
2023-01-14 11:19:32 -0500

    Implemented rotation matching with sampling over time. It is very rustic and not fully tested, but it works with the tests I made with dummy cubes.

--- Assets/RotationMatching/Scripts/MatchingAngles.cs
+++ Assets/RotationMatching/Scripts/MatchingAngles.cs
@@ -5,16 +5,21 @@ public class MatchingAngles : MonoBehaviour
 
     [SerializeField] private GameObject object1, object2;
 
-    [SerializeField] private float currentValue = 0f;
+    [SerializeField] private float currentDistance = 0f;
+    [SerializeField] private float prevDistance = 0f;
     [SerializeField] private float referenceValue = 0f;
 
     [Range(.001f, 1f)]
     [SerializeField] private float threashold = .1f;
 
-    private int sample = 0;
-    private int maxSample = 500;
-    private float prevValue = 0f;
+    private int match = 0;
+    private int matchMax = 500;
 
+    private int samplingInterval = 500;
+    private int sampling = 0;
+
+
+    private bool matched = false;
 
     void Start()
     {
@@ -23,37 +28,51 @@ public class MatchingAngles : MonoBehaviour
     void Update()
     {
 
-        //if (Input.GetKeyDown(KeyCode.V))
-        //    referenceValue = currentValue;
 
         var obj1matrix = Matrix4x4.TRS(new Vector3(0, 0, 0), object1.transform.rotation, new Vector3(1.0f, 1.0f, 1.0f));
         var obj2matrix = Matrix4x4.TRS(new Vector3(0, 0, 0), object2.transform.rotation, new Vector3(1.0f, 1.0f, 1.0f));
 
-        currentValue = Utils.DistMatrices(obj1matrix, obj2matrix);
-        
-        var absdiff = Mathf.Abs(referenceValue - currentValue);
+        currentDistance = Utils.DistMatrices(obj1matrix, obj2matrix);
 
-        prevValue = currentValue;
-        var absdiffCurrPrev = Mathf.Abs(prevValue - currentValue);
+        var absdiffCurrPrev = Mathf.Abs(prevDistance - currentDistance);
 
 
-        if (absdiffCurrPrev < threashold)
-            sample++;
-        else
-            sample = 0;
-
-        Debug.Log(sample);
-
-        if (sample > maxSample)
-            referenceValue = currentValue;
-
-        if (absdiff > threashold)
+        if (match > matchMax)
         {
-
-            Debug.Log("NOT MATCHING --- Diff: " + absdiff);
-            return;
+            referenceValue = currentDistance;
+            matched = true;
+            match = 0;
         }
 
-        Debug.Log("    MATCHING --- Diff: " + absdiff);
+        if (!matched)
+        {
+            if (absdiffCurrPrev < threashold)
+            {
+                match++;
+                Debug.Log("SAMPLING");
+            }
+            else
+            {
+                match = 0;
+                Debug.Log("NOT MATCHING");
+            }
+
+            if (sampling % samplingInterval == 0)
+                prevDistance = currentDistance;
+
+            sampling++;
+ 
+        }
+        else
+        {
+            
+            var currentAbsdiff = Mathf.Abs(referenceValue - currentDistance);
+
+            if (currentAbsdiff > threashold)
+                matched = false;
+            Debug.Log("    MATCHING --- Diff: " + currentAbsdiff);
+
+        }
+
     }
 }
